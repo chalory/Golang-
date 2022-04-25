b898d316c8d7627b1c8cd328c068b1d5fbc3e6e1
Cloudy Rain <57855670+chalory@users.noreply.github.com>
2022-04-25 20:58:10 +0800

    manipulating values in go

---
+++ values.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	fmt.Println("chantal" + "rain")
+	// strings can be added together using +
+
+	fmt.Println("1+1 =", 1+1)
+	res_1 := math.Round(7.0 / 3.0)
+	fmt.Println("The rounded answer to the equation of 7.0/3.0 is =", res_1)
+	// strings and integers
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	// boolean and boolean operators
+}
